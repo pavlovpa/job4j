c6ba8afa3be0cc97110e2bfb6caa1d186a0cc7f1
Petr-Pavlov <pavlov.p@hotmail.com>
2021-01-29 14:05:25 +0300

    6.4. Слово начинается с ...[#148810]

--- chapter_001/src/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/ru/job4j/array/ArrayChar.java
@@ -6,6 +6,7 @@ public class ArrayChar {
         for (int i = 0; i != pref.length; i++) {
             if (word[i] != pref[i]) {
                 result = false;
+                break;
             }
         }
         return result;
