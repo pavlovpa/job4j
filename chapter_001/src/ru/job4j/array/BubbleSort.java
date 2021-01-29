1e7da56e6ee5079881d8701dee1f85dcfa14088a
Petr-Pavlov <pavlov.p@hotmail.com>
2021-01-29 14:41:08 +0300

    6.5. Создать программу для сортировки массива методом перестановки.[#148814]

--- chapter_001/src/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/ru/job4j/array/BubbleSort.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class BubbleSort {
     public static int[] sort(int[] array) {
-        for (int j = 0; j != array.length - 2; j++) {
+        for (int j = 0; j != array.length - 1; j++) {
             for (int i = 0; i != array.length - 1; i++) {
                 if (array[i] > array[i + 1]) {
                     int temp = array[i];
--- chapter_001/test/ru.job4j.array/BubbleSortTest.java
+++ chapter_001/test/ru.job4j.array/BubbleSortTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
 public class BubbleSortTest {
     @Test
     public void whenSortArrayWithTenElementsThenSortedArray() {
-        int[] rawArray = {1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
+        int[] rawArray = {1, 5, 4, 2, 3, 1, 7, 8, 5, 0};
         int[] resultArray = BubbleSort.sort(rawArray);
         int[] expectArray = {0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
         assertThat(resultArray, is(expectArray));
