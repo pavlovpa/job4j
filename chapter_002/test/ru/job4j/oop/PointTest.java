10aade9967a266cb0e9646a96e39048304370e60
Petr-Pavlov <pavlov.p@hotmail.com>
2021-03-03 08:27:40 +0300

    1. Рефакторинг - Расстояние между точками.[#148906] добавлен тест на двумерное расстояние

--- chapter_002/test/ru/job4j/oop/PointTest.java
+++ chapter_002/test/ru/job4j/oop/PointTest.java
@@ -7,6 +7,14 @@ import static org.junit.Assert.assertThat;
 
 public class PointTest {
     @Test
+    public void distanceTest() {
+        Point a = new Point(0, 10);
+        Point b = new Point(0, 0);
+        double rsl = b.distance(a);
+        assertThat(rsl, closeTo(10, 0.001));
+    }
+
+    @Test
     public void distance3DTest() {
         Point a = new Point(0, 0, 0);
         Point b = new Point(4, 0, 0);
